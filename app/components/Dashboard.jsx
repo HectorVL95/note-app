2b98aa1bd4d2f116b0537d07bdc91c02b38a05dc
Hector <francisco1495@gmail.com>
2024-07-17 14:58:43 -0500

    se agrega la funcionalidad para subirt y borrar imagenes

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -12,15 +12,13 @@ import { addDoc, getDocs, collection, deleteDoc, doc, updateDoc } from "firebase
 import { db, auth } from "../server/firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { ref, uploadBytes, listAll, list, getDownloadURL, deleteObject } from "firebase/storage";
+import { ref, uploadBytes, list, getDownloadURL, deleteObject } from "firebase/storage";
 import { storage } from "../server/firebase";
 import { v4 } from "uuid";
-import { Darumadrop_One } from "next/font/google";
-
 
 const Dashboard = () => {
   
-  const [notes, setNotes] = useState({title: '', body: ''});
+  const [notes, setNotes] = useState({ title: '', body: '' });
   const [user, setUser] = useState(null);
   const [noteList, setNoteList] = useState([]);
   const [editSavedNote, setEditSavedNote] = useState(null);
@@ -31,34 +29,33 @@ const Dashboard = () => {
   const imagesListRef = ref(storage, "images/");
 
   useEffect(() => {
-    onAuthStateChanged (auth, (user) => {
+    onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
         getNoteList(user.uid);
       } else {
-        setUser(null),
-        setNoteList([])
+        setUser(null);
+        setNoteList([]);
       }
-    })
+    });
 
     list(imagesListRef).then((response) => {
       response.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
           setImageUrls((prev) => [...prev, url]);
-        })
-      })
-    })
-  }, [])
+        });
+      });
+    });
+  }, []);
 
   const getNoteList = async (uid) => {
-    try{
-      const userNotesCollectionRef = collection(db, 'user', uid, 'notes')
-      const notesSnapshop = await getDocs(userNotesCollectionRef)
-      const notes = notesSnapshop.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-      setNoteList(notes)
-    }
-    catch (err) {
-      console.error(err)
+    try {
+      const userNotesCollectionRef = collection(db, 'user', uid, 'notes');
+      const notesSnapshot = await getDocs(userNotesCollectionRef);
+      const notes = notesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setNoteList(notes);
+    } catch (err) {
+      console.error(err);
     }
   }
 
@@ -73,38 +70,36 @@ const Dashboard = () => {
           await uploadBytes(imageRef, imageUpload);
           imageUrl = await getDownloadURL(imageRef);
         }
-        const newNote = { ...notes, imageUrl};
-        const docRef = await addDoc(userNotesCollectionRef, newNote)
-        setNoteList(prevNotes => [...prevNotes, {id: docRef.id, ...newNote}]);
-        setNotes({ title: '', body: ''});
-        setImageUpload(null)
-      } 
-      else{
-        console.error('User not logged in')
+        const newNote = { ...notes, imageUrl };
+        const docRef = await addDoc(userNotesCollectionRef, newNote);
+        setNoteList(prevNotes => [...prevNotes, { id: docRef.id, ...newNote }]);
+        setNotes({ title: '', body: '' });
+        setImageUpload(null);
+      } else {
+        console.error('User not logged in');
       }
-      
-    }
-    catch (err) {
-      console.error(err)
+    } catch (err) {
+      console.error(err);
     }
   }
- 
-  const deletePic = async () => {
+
+  const deletePic = async (id, imageUrl) => {
     try {
       if (!user) {
         console.error('User not logged in');
         return;
       }
 
-      if (!imageUpload) {
-        console.error('No image to delete');
-        return;
-      }
-
-      const uploadedImagePath = `images/${imageUpload.name}`;
-      const imageRef = ref(storage, uploadedImagePath);
+      const imageRef = ref(storage, imageUrl);
       await deleteObject(imageRef);
       console.log('Deleted Successfully');
+
+      // Update the note state to remove the imageUrl
+      const docRef = doc(db, 'user', user.uid, 'notes', id);
+      await updateDoc(docRef, { imageUrl: '' });
+      setNoteList(prevNotes => prevNotes.map(note =>
+        note.id === id ? { ...note, imageUrl: '' } : note
+      ));
     } catch (err) {
       console.error('Error deleting image:', err);
     }
@@ -120,21 +115,23 @@ const Dashboard = () => {
       } else {
         console.error('User not logged in');
       }
-    } 
-    catch (err) {
-      console.error('Error updating note', err)
+    } catch (err) {
+      console.error('Error updating note', err);
     }
   }
 
-  const handleDelete = async (id) => {
-    try{
+  const handleDelete = async (id, imageUrl) => {
+    try {
       if (user) {
         const docRef = doc(db, 'user', user.uid, 'notes', id);
         await deleteDoc(docRef);
-        console.log(`Deleted note wih id ${id}`)
+        if (imageUrl) {
+          await deletePic(id, imageUrl);
+        }
+        console.log(`Deleted note with id ${id}`);
         setNoteList(prevNotes => prevNotes.filter(note => note.id !== id));
       } else {
-        console.error('user not logged in');
+        console.error('User not logged in');
       }
     } catch (err) {
       console.error('Error deleting note', err);
@@ -143,14 +140,14 @@ const Dashboard = () => {
 
   const handleLogOut = () => {
     signOut(auth).then(() => {
-      router.push('/')
-    })
+      router.push('/');
+    });
   }
+
   const clearNotes = () => {
     console.log('Clearing notes');
     setNotes({ title: '', body: '' });
   };
-
 
   return (
     <Box component="div" className="flex flex-col">
@@ -162,76 +159,77 @@ const Dashboard = () => {
               <IconButton onClick={handleSave}>
                 <IoMdAdd />
               </IconButton>
-              <IconButton onClick={clearNotes} >
+              <IconButton onClick={clearNotes}>
                 <TbHttpDelete />
               </IconButton>
-              
             </Box>
-            <TextField 
-              onChange={(e) => setNotes({...notes, title: e.target.value})}
+            <TextField
+              onChange={(e) => setNotes({ ...notes, title: e.target.value })}
               value={notes.title}
-              />
-            <TextField 
-              onChange={(e) => setNotes({...notes, body: e.target.value})}
+            />
+            <TextField
+              onChange={(e) => setNotes({ ...notes, body: e.target.value })}
               value={notes.body}
-              />
-              <input type="file" onChange={(e) => {
-                setImageUpload(e.target.files[0]);
-              }}/>
+            />
+            <input type="file" onChange={(e) => {
+              setImageUpload(e.target.files[0]);
+            }} />
           </FormControl>
           <Button variant="contained" onClick={handleLogOut}>Log Out</Button>
         </Box>
-        {noteList.map(data => 
+        {noteList.map(data =>
           <Box key={data.id} className="flex flex-col">
             <Box className="flex">
               {editSavedNote === data.id ?
-              <IconButton onClick={() => saveEdit(data.id, { title: data.title, body: data.body })}>
-                <FaRegSave />
-              </IconButton> 
-              :
-              <IconButton onClick={() =>  setEditSavedNote(data.id)}>
-                <MdOutlineEdit/> 
-              </IconButton>
-              
+                <IconButton onClick={() => saveEdit(data.id, { title: data.title, body: data.body })}>
+                  <FaRegSave />
+                </IconButton>
+                :
+                <IconButton onClick={() => setEditSavedNote(data.id)}>
+                  <MdOutlineEdit />
+                </IconButton>
               }
-              <IconButton onClick={()=> handleDelete(data.id)}>
-                <FaTrashAlt/> 
+              <IconButton onClick={() => handleDelete(data.id, data.imageUrl)}>
+                <FaTrashAlt />
               </IconButton>
             </Box>
-            {editSavedNote === data.id ? 
-            <>
-              <TextField
-                onChange={(e) => setNoteList(prevNotes => prevNotes.map(note => note.id === data.id ? { ...note, title: e.target.value } : note))}
-                value={data.title}
-                placeholder="Title"
-              />
-              <TextField
-                onChange={(e) => setNoteList(prevNotes => prevNotes.map(note => note.id === data.id ? { ...note, body: e.target.value } : note))}
-                value={data.body}
-                placeholder="Body"
-              />
-              {data.imageUrl &&
-              
-              <Box component="div">
-                <IconButton>
-                <TiDelete onClick={deletePic}/>
-                </IconButton>
-                <img src={data.imageUrl} alt="Note Image"/>
-              </Box>
-              }
-            </>
-            :
-            <>
-              <Box>{data.title}</Box>
-              <Box>{data.body}</Box>
-              {data.imageUrl && <img src={data.imageUrl} alt="Note Image"/>}
-            </>
+            {editSavedNote === data.id ?
+              <>
+                <TextField
+                  onChange={(e) => setNoteList(prevNotes => prevNotes.map(note => note.id === data.id ? { ...note, title: e.target.value } : note))}
+                  value={data.title}
+                  placeholder="Title"
+                />
+                <TextField
+                  onChange={(e) => setNoteList(prevNotes => prevNotes.map(note => note.id === data.id ? { ...note, body: e.target.value } : note))}
+                  value={data.body}
+                  placeholder="Body"
+                />
+                {data.imageUrl &&
+                  <Box component="div">
+                    <>
+                      <IconButton onClick={() => deletePic(data.id, data.imageUrl)}>
+                        <TiDelete />
+                      </IconButton>
+                      <img src={data.imageUrl} alt="Note Image" />
+                    </>
+                  </Box>
+                }
+              </>
+              :
+              <>
+                <Box>{data.title}</Box>
+                <Box>{data.body}</Box>
+                {data.imageUrl && (
+                  <Box component="div">
+                    <img src={data.imageUrl} alt="Note Image" />
+                  </Box>
+                )}
+              </>
             }
           </Box>
-          )
-        }
+        )}
       </Box>
-
     </Box>
   );
 }
